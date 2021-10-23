3dca48d9214738d82a777f1ee1c5dc3ee4b87103
PaurashKumar <paurashdewangan>
2021-10-23 13:05:37 +0530

    ok done for 9 table

--- Myjava1.java
+++ Myjava1.java
@@ -2,7 +2,7 @@ class Myjava1{
     public static void main(String[] args) {
 
         for(int i=1;i<=10;i++){
-            System.out.println(i*7);
+            System.out.println(i*9);
         }
          
     }
